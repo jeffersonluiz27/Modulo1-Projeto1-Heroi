5ca1a21ab81bfba6900285723b5e05651ea3481c
jeffersonluiz27 <developer.jefferson27@outlook.com>
2022-01-24 15:34:22 -0300

    Atualizando

--- script.js
+++ script.js
@@ -7,135 +7,151 @@
 // Chamada do prompt
 const prompt = require('prompt-sync')();
 
-// Declarando Variaveis Principais
-var answersY = 0; //Respostas SIM
-var question; //Perguntas
 
-// Apresentando Background da história
-console.log();
-console.log(`
-Você é um personagem de uma história em quadrinhos,
-mas você não é o personagem principal.
-Você então decide se tornar o herói da história.
-Percorre uma longa jornada enfrentando diversos desafios
-e luta contra diversos personagens,
-para então conseguir chegar na capa
-e colocar o seu nome no título da história.\n
-`);
+do {
+  // Declarando Variaveis Principais
+  var answersY = 0; //Respostas SIM
+  var question; //Perguntas
+  var newGame = '';
 
-// Instrução
-console.log(`
-Responda as perguntas a seguir para descobrir se você chegou 
-no seu objetivo. Use (S) para Sim e (N) para Não.\n
-`);
+  // Apresentando Background da história
+  console.log();
+  console.log(`
+  Auryon é um personagem de uma história em quadrinhos, mas ele não é o personagem principal.
+  O grandioso Lupan é o heroi cujo o nome está na capa, porém Lupan é um heroi que se aproveita 
+  da boa vontade das pessoas para conseguir mais glorias. Indignado Auryon deseja fazer alguma 
+  coisa para o bem dos outros personagens. Lucius seu melhor amigo disse a Auryon que para se 
+  tornar um heroi, ele precisava ter seu nome escrito na capa! Auryon então decide se tornar o 
+  herói da história, mas isso não vai ser facil, porque o autor da hitória não pode descobrir 
+  o que ele está fazendo. Auryon então percorre uma longa jornada enfrentando diversos desafios 
+  e lutando contra diversos personagens, para então conseguir chegar na capa e colocar o seu nome 
+  no título da história sem que o autor descubra.\n
+  `);
 
-// Pergunta 1
-question = '';                                 // Zerando a variavel
-while (question != 'S' && question != 'N') {   // Loop exibe a pergunta até o usuario digitar uma opção válida
-  question = prompt(
-    'Você encontrou o número da página em que você aparece? (S/N): '
-  ).toUpperCase();                             // Convertendo a entrada para maiuscula
-  if (question != 'S' && question != 'N') {    // Verificando a entrada e exibindo erro se não corresponder como o esperado 
-    console.log('Opção invalida! Por favor escolha uma opção válida!');
+  // Instrução
+  console.log(`
+  Responda as perguntas a seguir para descobrir se Auryon 
+  chegou no seu objetivo. Use (S) para Sim e (N) para Não.\n
+  `);
+
+  // Pergunta 1
+  question = ''; // Zerando a variavel
+  while (question != 'S' && question != 'N') {
+    // Loop exibe a pergunta até o usuario digitar uma opção válida
+    question = prompt(
+      'Auryon descobriu como saltar as páginas? (S/N): '
+    ).toUpperCase(); // Convertendo a entrada para maiuscula
+    if (question != 'S' && question != 'N') {
+      // Verificando a entrada e exibindo erro se não corresponder como o esperado
+      console.log('Opção invalida! Use (S) para Sim e (N) para Não.');
+    } else if (question == 'S') {
+      // Verifica se o usuario escolheu SIM e contabiliza
+      answersY++;
+    }
   }
-}
 
-if (question == 'S') {                         // Verifica se o usuario escolheu SIM e contabiliza
-  answersY = answersY + 1;
-}
-
-//Pergunta 2
-question = ''; // Zerando a variavel
-while (question != 'S' && question != 'N') {
-  question = prompt(
-    'Você identificou em que direção fica a capa? (S/N): '
-  ).toUpperCase();
-  if (question != 'S' && question != 'N') {
-    console.log('Opção invalida! Por favor escolha uma opção válida!');
+  //Pergunta 2
+  question = ''; // Zerando a variavel
+  while (question != 'S' && question != 'N') {
+    question = prompt(
+      'Auryon descobriu quem são seus aliados? (S/N): '
+    ).toUpperCase();
+    if (question != 'S' && question != 'N') {
+      console.log('Opção invalida! Use (S) para Sim e (N) para Não.');
+    } else if (question == 'S') {
+      answersY++;
+    }
   }
-}
 
-if (question == 'S') {
-  answersY = answersY + 1;
-}
-
-//Pergunta 3
-question = '';
-while (question != 'S' && question != 'N') {
-  question = prompt(
-    'Você descobriu como saltar as páginas? (S/N): '
-  ).toUpperCase();
-  if (question != 'S' && question != 'N') {
-    console.log('Opção invalida! Por favor escolha uma opção válida!');
+  //Pergunta 3
+  question = '';
+  while (question != 'S' && question != 'N') {
+    question = prompt('Auryon conseguiu derrotar Lupan? (S/N): ').toUpperCase();
+    if (question != 'S' && question != 'N') {
+      console.log('Opção invalida! Use (S) para Sim e (N) para Não.');
+    } else if (question == 'S') {
+      answersY++;
+    }
   }
-}
 
-if (question == 'S') {
-  answersY = answersY + 1;
-}
-
-//Pergunta 4
-question = '';
-while (question != 'S' && question != 'N') {
-  question = prompt(
-    'Você descobriu quem são seus aliados? (S/N): '
-  ).toUpperCase();
-  if (question != 'S' && question != 'N') {
-    console.log('Opção invalida! Por favor escolha uma opção válida!');
+  //Pergunta 4
+  question = '';
+  while (question != 'S' && question != 'N') {
+    question = prompt(
+      'Auryon lembrou de pegar um pincel? (S/N): '
+    ).toUpperCase();
+    if (question != 'S' && question != 'N') {
+      console.log('Opção invalida! Use (S) para Sim e (N) para Não.');
+    } else if (question == 'S') {
+      answersY++;
+    }
   }
-}
 
-if (question == 'S') {
-  answersY = answersY + 1;
-}
-
-//Pergunta 5
-question = '';
-while (question != 'S' && question != 'N') {
-  question = prompt('Você lembrou de pegar um pincel? (S/N): ').toUpperCase();
-  if (question != 'S' && question != 'N') {
-    console.log('Opção invalida! Por favor escolha uma opção válida!');
+  //Pergunta 5
+  question = '';
+  while (question != 'S' && question != 'N') {
+    question = prompt(
+      'Auryon encontrou o frasco de tinta? (S/N): '
+    ).toUpperCase();
+    if (question != 'S' && question != 'N') {
+      console.log('Opção invalida! Use (S) para Sim e (N) para Não.');
+    } else if (question == 'S') {
+      answersY++;
+    }
   }
-}
 
-if (question == 'S') {
-  answersY = answersY + 1;
-}
-
-//Pergunta 6
-question = '';
-while (question != 'S' && question != 'N') {
-  question = prompt('Você encontrou o frasco de tinta? (S/N): ').toUpperCase();
-  if (question != 'S' && question != 'N') {
-    console.log('Opção invalida! Por favor escolha uma opção válida!');
+  //Pergunta 6
+  question = '';
+  while (question != 'S' && question != 'N') {
+    question = prompt(
+      'Auryon conseguiu se esconder da sentinela? (S/N): '
+    ).toUpperCase();
+    if (question != 'S' && question != 'N') {
+      console.log('Opção invalida! Use (S) para Sim e (N) para Não.');
+    } else if (question == 'S') {
+      answersY++;
+    }
   }
-}
 
-if (question == 'S') {
-  answersY = answersY + 1;
-}
-console.log();
+  console.log();
 
-// Exibindo os resultados
-if (answersY == 0) {
-  console.log('Que pena! Você falhou!! \nE ainda foi apagado da história!');
-} else if (answersY <= 2) {
-  console.log(
-    'Que pena! Você falhou!!  \nMas felizmente você se safou de ser apagado!'
-  );
-} else if (answersY <= 4) {
-  console.log(
-    'Ops!! Você quase conseguiu! \nMas o autor te descobriu antes de você chegar lá!'
-  );
-} else if (answersY == 5) {
-  console.log(
-    'Você conseguiu! \nFoi sofrido chegar até aqui, mas dava pra fazer melhor!!'
-  );
-} else if (answersY == 6) {
-  console.log(
-    'Parabéns!! \nVocê alcançou seu objetivo com perfeição. \nForam tão notáveis seus feitos, \nque o autor decidiu fazer uma serie de histórias sobre você!!!'
-  );
-} else {
-  console.log('Ops! Algo Deu Errado');
-}
-console.log('');
+  // Exibindo os resultados
+  if (answersY == 0) {
+    console.log('Que pena, Auryon falhou!! \nE ainda foi apagado da história!');
+  } else if (answersY <= 2) {
+    console.log(
+      'Que pena, Auryon falhou!! \nMas felizmente ele se safou de ser apagado! \nO Autor colocou ele na ultima página.'
+    );
+  } else if (answersY <= 4) {
+    console.log(
+      'Ops!! Auryon quase conseguiu! \nMas o autor descobriu e impediu Auryon de chagar na capa!'
+    );
+  } else if (answersY == 5) {
+    console.log(
+      'Auryon conseguiu! \nFoi sofrido chegar até aqui, mas dava pra fazer melhor! \nO Autor não gostou muito da mudança!'
+    );
+  } else if (answersY == 6) {
+    console.log(
+      'Parabéns!! \nAuryon alcançou seu objetivo com perfeição. \nForam tão notáveis seus feitos, \nque o autor decidiu fazer uma série de histórias sobre Auryon!!!'
+    );
+  }
+  console.log('');
+
+  do {
+    newGame = prompt(
+      'Quer jogar de novo?: Use (S) para Sim e (N) para Não (SAIR). '
+    ).toUpperCase();
+    if (newGame != 'S' && newGame != 'N') {
+      console.log('Opção invalida! Use (S) para Sim e (N) para Não.');
+    } else if (question == 'S') {
+      if (newGame == `S`) {
+        console.clear();
+        console.log();
+      } else {
+        console.clear();
+        console.log('Obrigado por jogar!');
+        console.log();
+      }
+    }
+  } while (newGame != 'S' && newGame != 'N');
+} while (newGame == 'S');
